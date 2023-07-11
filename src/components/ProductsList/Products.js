fc4a5311a58b039194d7c3895c520afbe81d1bf4
Saekyul <saekyulyang@gmail.com>
2023-07-11 23:10:55 +0900

    상품 컴포넌트 디자인

--- src/components/ProductsList/Products.js
+++ src/components/ProductsList/Products.js
@@ -1,16 +1,32 @@
 import BookmarkButton from "../Buttons/BookmarkButton";
 import Styled from "styled-components";
+import { ImageContainer, ProductName, StyledImage } from "./ProductsStyles";
 
-const ImageContainer = Styled.div`
-  position: relative;
-  display: inline-block;
+const ProductContainer = Styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: 287px;
 `;
 
-const StyledImage = Styled.img`
-  width: 200px;
-  height: 200px;
-  
+const PriceContainer = Styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: right;
 `;
+
+const ProductDiscountPercentage = Styled.div`
+  font-size: 16px;
+  font-weight: 800;
+  color: #452CDD;
+`;
+
+const ProductPrice = Styled.div`
+  font-size: 16px;
+`;
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 const Products = ({ item }) => (
   <div>
@@ -18,9 +34,15 @@ const Products = ({ item }) => (
       <StyledImage src={item.image_url} alt={item.title} />
       <BookmarkButton />
     </ImageContainer>
-    <h2>{item.title}</h2>
-    <p>{item.price}</p>
-    <p>{item.discountPercentage}</p>
+    <ProductContainer>
+      <ProductName>{item.title}</ProductName>
+      <PriceContainer>
+        <ProductDiscountPercentage>
+          {item.discountPercentage} %
+        </ProductDiscountPercentage>
+        <ProductPrice>{numberWithCommas(item.price)} 원</ProductPrice>
+      </PriceContainer>
+    </ProductContainer>
   </div>
 );
 
---
+++ src/components/ProductsList/ProductsStyles.js
@@ -0,0 +1,17 @@
+import Styled from "styled-components";
+
+export const ImageContainer = Styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+export const ProductName = Styled.div`
+  font-size: 16px;
+  font-weight: 800;
+`;
+
+export const StyledImage = Styled.img`
+  width: 287px;
+  height: 228px;
+  border-radius: 10%;
+`;

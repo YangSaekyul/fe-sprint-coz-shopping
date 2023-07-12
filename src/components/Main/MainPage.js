62d638369eda5db475973b74f08bb11a3037730a
Saekyul <saekyulyang@gmail.com>
2023-07-12 19:01:27 +0900

    상품 리스트 조회 / 메인페이지에선 필터 안보이게 수정

--- src/components/Main/MainPage.js
+++ src/components/Main/MainPage.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import ProductComponent from "../ProductsPage/ProductsPage";
+
 const ListTitle = styled.h1`
   margin: 40px;
 `;
@@ -8,6 +10,7 @@ function MainPage() {
   return (
     <>
       <ListTitle>상품 리스트</ListTitle>
+      <ProductComponent numItems={4} showFilter={false} />
       <ListTitle>북마크 리스트</ListTitle>
     </>
   );
